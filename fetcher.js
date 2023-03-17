75026c9c14b7d9365f1e2205b9103dfd103d2096
Yonin408 <justin12689@gmail.com>
2023-03-16 21:53:55 -0700

    used eslint to make code more readable

--- fetcher.js
+++ fetcher.js
@@ -1,19 +1,19 @@
-const fs= require('fs');
+const fs = require('fs');
 const request = require('request');
-const [url, path] = process.argv.slice(2)
+const [url, path] = process.argv.slice(2);
 let bytes = 0;
 
 
 request.get(url, (error, resource, body) => {
-   if (error){
+  if (error) {
     ('error', error);
     return;
-   }
-    bytes += body.length;
-    fs.writeFile(path, body, err => {
-        if (err) {
-            console.error(err);
-        }
-        console.log(`Downloaded and saved ${bytes} bytes to ${path}`)
-    })
+  }
+  bytes += body.length;
+  fs.writeFile(path, body, err => {
+    if (err) {
+      console.error(err);
+    }
+    console.log(`Downloaded and saved ${bytes} bytes to ${path}`);
+  });
 });
